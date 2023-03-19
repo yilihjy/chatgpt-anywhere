87080fb2c8f64589779d2f7db655c70b8d0b9b1b
yilihjy <yilihjy@gmail.com>
2023-03-19 15:45:08 +0800

    build:修改输出目录

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ import IconsResolver from 'unplugin-icons/resolver'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/chatgpt-anywhere/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
     AutoImport({
